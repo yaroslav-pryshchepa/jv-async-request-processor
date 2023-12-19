bb553ff5fa901f4d33d06003e58e23f6a37c3d74
boroda4436 <chupika.bohdan@ukr.net>
2023-12-19 12:44:27 +0200

    fixed task description

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,13 +1,9 @@
 package mate.academy;
 
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
-import java.util.concurrent.TimeUnit;
 
 public class AsyncRequestProcessor {
-    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
     private final Executor executor;
 
     public AsyncRequestProcessor(Executor executor) {
@@ -15,24 +11,6 @@ public class AsyncRequestProcessor {
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        // Return cached result if available
-        UserData cachedData = cache.get(userId);
-        if (cachedData != null) {
-            return CompletableFuture.completedFuture(cachedData);
-        }
-
-        // Process request asynchronously if not cached
-        return CompletableFuture.supplyAsync(() -> {
-            // Simulate time-consuming request processing
-            try {
-                TimeUnit.SECONDS.sleep(1); // simulate delay
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-
-            UserData userData = new UserData(userId, "Details for " + userId);
-            cache.putIfAbsent(userId, userData); // use putIfAbsent for thread safety
-            return userData;
-        }, executor);
+        return null;
     }
 }
