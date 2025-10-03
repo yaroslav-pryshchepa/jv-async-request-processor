b754ff8c1ce1a05041c42afc44b51d219c2d8ea7
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-10-03 22:09:42 +0300

    added fixes

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,7 +1,6 @@
 package mate.academy;
 
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
@@ -9,45 +8,25 @@ import java.util.concurrent.Executor;
 public class AsyncRequestProcessor {
 
     private final Executor executor;
-    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
+    private final Map<String, CompletableFuture<UserData>> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        if (cache.containsKey(userId)) {
-            return CompletableFuture.completedFuture(cache.get(userId));
-        }
-        CompletableFuture<UserData> completableFuture = this.create(userId);
-        completableFuture = completableFuture.thenApply(userData -> {
-            cache.put(userId, userData);
-            return userData;
-        });
-        return completableFuture;
+        return cache.computeIfAbsent(userId, id -> create(id));
     }
 
-    public CompletableFuture<UserData> create(String userId) {
+    private CompletableFuture<UserData> create(String userId) {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(1000);
-                String details = generateRandomString(10);
+                String details = "Details for " + userId;
                 return new UserData(userId, details);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }, executor);
-    }
-
-    private static String generateRandomString(int length) {
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        Random random = new Random();
-        StringBuilder sb = new StringBuilder(length);
-
-        for (int i = 0; i < length; i++) {
-            int index = random.nextInt(alphabet.length());
-            sb.append(alphabet.charAt(index));
-        }
-        return sb.toString();
     }
 }
